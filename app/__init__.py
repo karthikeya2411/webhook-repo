fbf0346c9f28f41a8da92e2d8f7f5406d17378a0
gade.preethi <gade.preethi@accenture.com>
2026-01-30 21:32:40 +0530

    Added gunicorn for deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
-
 from app.webhook.routes import webhook
+from app.extensions import init_mongo
 
 
-# Creating our flask app
 def create_app():
 
     app = Flask(__name__)
-    
-    # registering all the blueprints
+
+    # Initialize MongoDB
+    init_mongo(app)
+
+    # Register blueprint
     app.register_blueprint(webhook)
-    
+
     return app
--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,7 @@
 from flask_pymongo import PyMongo
 
-# Setup MongoDB here
-# mongo = PyMongo(uri="mongodb://localhost:27017/database")
+mongo = PyMongo()
+
+def init_mongo(app):
+    app.config["MONGO_URI"] = "mongodb+srv://preethihja_db_user:fgvtYxdflQ8MmRty@cluster0.flmdnv4.mongodb.net/github_events?retryWrites=true&w=majority"
+    mongo.init_app(app)
